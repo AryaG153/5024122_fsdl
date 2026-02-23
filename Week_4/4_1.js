1a39ea3941278ec6157eed9bffa77df184891b54
AryaG153 <thearyagunjotikar@gmail.com>
2026-02-23 03:42:43 +0000

    Week_4

---
+++ Week_4/4_1.js
@@ -0,0 +1,13 @@
+// Simple JavaScript code example
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+const message = greet("World");
+console.log(message);
+
+// Simple array operations
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);
